fdbeb1b8acd6ac8d8f25c5c424dbaa8f03fda249
Matheus Pereira <mathlimapereira@gmail.com>
2025-05-04 20:03:16 -0300

    feat: add draft frontend

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou especifique "http://localhost" se quiser restringir
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
